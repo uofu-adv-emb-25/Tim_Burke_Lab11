def0b26688f48d15f8825dc87c12c1b3da6796e7
Timothy Felt <timothyfelt@MacBook-Pro-7.local>
2025-11-12 12:57:42 -0700

    Changed Bluetooth name.

---
+++ build/server/CMakeFiles/lab11_server.dir/Users/timothyfelt/pico-sdk/lib/btstack/platform/embedded/hci_dump_segger_rtt_stdout.c.o.d
@@ -0,0 +1,3 @@
+server/CMakeFiles/lab11_server.dir/Users/timothyfelt/pico-sdk/lib/btstack/platform/embedded/hci_dump_segger_rtt_stdout.c.o: \
+ /Users/timothyfelt/pico-sdk/lib/btstack/platform/embedded/hci_dump_segger_rtt_stdout.c \
+ /Users/timothyfelt/Tim_Burke_Lab11/include/btstack_config.h
---
+++ build/server/CMakeFiles/lab11_server.dir/Users/timothyfelt/pico-sdk/lib/btstack/src/btstack_run_loop_base.c.o.d
@@ -0,0 +1,2 @@
+server/CMakeFiles/lab11_server.dir/Users/timothyfelt/pico-sdk/lib/btstack/src/btstack_run_loop_base.c.o: \
+ /Users/timothyfelt/pico-sdk/lib/btstack/src/btstack_run_loop_base.c
